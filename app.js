60c80c7258edb8d057972176de6fc07c520dea54
Simone <startae14@gmail.com>
2022-03-10 17:55:27 +0100

    Exercise Importing Your Own Files

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const add = require('./utils');
+// const add = require('./utils');
 
-const sum = add(4, -2);
+// const sum = add(4, -2);
 
-console.log(sum);
+// console.log(sum);
+const getNotes = require('./notes');
+
+console.log(getNotes());
--- utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js');
-
-const name = 'Simone';
-
-const add = function(a, b) {
-  return a + b;
-}
-
-module.exports = add;
