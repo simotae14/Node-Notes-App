fab0c4f851a4a43726c9974894359cb7ad9a8231
Simone <startae14@gmail.com>
2022-03-11 12:16:15 +0100

    create npm project and importing and use npm dependencies

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// const add = require('./utils');
-
-// const sum = add(4, -2);
-
-// console.log(sum);
+const validator = require('validator');
 const getNotes = require('./notes');
 
 console.log(getNotes());
+
+console.log(validator.isURL('https://mead.io'));
