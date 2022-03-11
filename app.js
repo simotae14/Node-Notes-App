fcd2aa486677531d4b68fcbd2ecdb5d3b29663a1
Simone <startae14@gmail.com>
2022-03-11 12:30:46 +0100

    Exercise: install and use the chalk package to Style your logs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const validator = require('validator');
+const chalk = require('chalk');
+// const validator = require('validator');
 const getNotes = require('./notes');
 
 console.log(getNotes());
 
-console.log(validator.isURL('https://mead.io'));
+// console.log(validator.isURL('https://mead.io'));
+console.log(chalk.green('Success!'));
+console.log(chalk.bold('Bold!'));
+console.log(chalk.bgGreen('Success Inversed!'));
