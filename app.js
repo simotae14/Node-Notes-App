494d41c947b2cac9eafb5c302731d5e11da931b8
Simone <startae14@gmail.com>
2022-03-17 00:44:56 +0100

    Adding a new Note

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes');
+const {
+  getNotes,
+  addNote,
+} = require('./notes');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -22,8 +25,7 @@ yargs.command({
     }
   },
   handler: function (argv) {
-    console.log('Title: ', argv.title);
-    console.log('Body: ', argv.body);
+    addNote(argv.title, argv.body);
   }
 })
 
--- notes.js
+++ notes.js
@@ -1,5 +1,41 @@
+const fs = require('fs');
+
 const getNotes = () => {
   return 'Your notes...';
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  // loading existing notes
+  const notes = loadNotes();
+  const duplicateNotes  = notes.filter(note => title === note.title);
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    });
+    saveNotes(notes);
+    console.log('New note added!');
+  } else {
+    console.log('Note title taken!');
+  }
+
+}
+
+const saveNotes = (notes) => {
+  fs.writeFileSync('notes.json', JSON.stringify(notes));
+}
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString(); // convert Buffer into string
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+}
+
+module.exports = {
+  getNotes,
+  addNote,
+};
